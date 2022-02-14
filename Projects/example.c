e78a8f58fd819bb699df5373b4b29b128695de2f
SFMSupersanta <otakuaria4710@gmail.com>
2022-02-14 20:18:54 +0700

    Update example.c

--- Projects/example.c
+++ Projects/example.c
@@ -1,17 +1,65 @@
-
-#include <stdio.h>
-#include <time.h>
-#include <conio.h>
-#include <ctype.h>
+/****************************************************************
+(▀̿Ĺ̯▀̿ ̿)
+Referring code made by SFMSupersanta.
+Program: Input validation
+version #
+****************************************************************/
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
+void clear (void) 
+{
+    while ( getchar() != '\n' );
+}
+//getchar(str, min, max)
+int GetInt(char msg[], int min, int max)
+{
+    int value;
+    int rc;
+    char ovflow;
+    while (true) 
+    {
+        printf("%s\n", msg);
+        printf("in the range [%d,%d] : ",min, max);
+        rc = scanf("%d%c",&value,&ovflow);
+        if(rc == 0)
+        {
+            printf("No input accepted");
+        }
+        else if(ovflow != '\n')
+        {
+            printf("Trailing characters");
+            clear();
+        }
+        else if(value < min || value > max)
+        {
+            printf("Out of range");
+        }
+        else break;
+    }
+    return value;
+}
 
 int main()
 {
-  double i= 3.464;
-  printf("%.2f\n", i - 0.005);
-  return 0;
+    srand(time(NULL));
+    int times = GetInt("Input",0,100);
+    int head=0, tail=0;
+    for (int i = 0; i < times; i++)
+    {
+        int coin = rand() % 2;
+        if (coin == 0)
+        {
+            head++;
+        }else
+        {
+            tail++;
+        }
+    }
+    printf("head result is %.2lf%%",(double)head*100/times);
+    printf("tail result is %.2lf%%",(double)tail*100/times);
+    return 0;
 }
