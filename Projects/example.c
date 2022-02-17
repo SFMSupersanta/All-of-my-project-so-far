695ea5f9bbb05bb8c20c9b5f9e7d66856aaad660
SFMSupersanta <otakuaria4710@gmail.com>
2022-02-17 12:13:25 +0700

    no more w6

--- Projects/example.c
+++ Projects/example.c
@@ -5,20 +5,12 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool checkdif(int arr[], int a, int b)
-{
-    for(int i = 0; i <b; i++) 
-    {
-        if(arr[i] == a)
-        return false;
-        break;
-    }
-    return true;
-}
-
 int main()
 {
-	int arr[] = {3,4,5,6};
-	printf( "%d\n",checkdif(arr,6,4) );
-	return 0;
+    while (true)
+    {
+	    printf("No coffe in cup\n");
+	    
+    }
+    return 0;
 }
