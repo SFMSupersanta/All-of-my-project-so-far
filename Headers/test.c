afd12ccb0ce1dde09c67d6fb336bae2a4fdfaf85
SFMSupersanta <otakuaria4710@gmail.com>
2022-02-24 11:15:47 +0700

    Headers update
    - Merge test folder to Headers folder.
    - Update Headers file:
    + Add odd and even integer validator.
    + getInt function update to be more efficient.

---
+++ Headers/test.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include "validator.h"
+
+ 
+
+
+int main()
+{   
+    double i = getDouble("TYPE: ",-100,1000);
+    printf("i: %lf\n", i); 
+    //printf("%C\n",atof("3lmaobruhbruh")-'0');
+    return 0;
+}
--- Headers/validator.h
+++ Headers/validator.h
@@ -8,8 +8,8 @@
      while ((c=getchar()) != '\n'&&c!=EOF);
  }
 
-//even val
-int getEven(char arr[],int min, int max)
+ //odd val
+ int getOdd(char arr[],int min, int max)
 {
     char val[20];           //input value
     int intval,count,rc;      //int value and count value
@@ -45,43 +45,98 @@ int getEven(char arr[],int min, int max)
             if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
             else if(min<intval&&intval<max)
             {
-                if(intval%2==0) return intval;
-                else printf("**Not an even number**\n");
+                if(intval%2!=0) return intval;
+                else printf("**Not an odd number**\n");
             }else printf("**OUT OF RANGE**\n");
         } else printf("**No input**\n");
     }
 }
 
+ //even val
+ int getEven(char arr[],int min, int max)
+ {
+     char val[20];           //input value
+     int intval,count,rc;      //int value and count value
+     while(1)
+     {
+         printf("%s", arr);
+         count = 0;
+         rc =0;
+         fgets(val,20,stdin);
+         if(val[0]!='\n')
+         {
+             if(0<max&&0>min) if(val[0]=='0' && val[1] == '\n') return 0;
+             for(int i=0; i< 20; i++) 
+             {
+                 if(val[i]=='\n') rc = 1;
+             } 
+             if(rc==0) clear_buffer();
+             intval = atoi(val);
+ 
+             //printf("%d\n", intval);
+ 
+             if(intval<=0) count++;
+ 
+             while(intval!=0)
+             {
+                 intval/=10;
+                 count++;
+             }
+ 
+             //printf("%d\n", count);
+ 
+             intval = atoi(val);
+             if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
+             else if(min<intval&&intval<max)
+             {
+                 if(intval%2==0) return intval;
+                 else printf("**Not an even number**\n");
+             }else printf("**OUT OF RANGE**\n");
+         } else printf("**No input**\n");
+     }
+ }
+
 long long getInt(char msg[], long long min, long long max) 
 {
-     long long value, rc;
-     int keeptrying = 1;
-     char excess;
-
-     do 
+     char val[20];           //input value
+     int intval,count,rc;      //int value and count value
+     while(1)
      {
-         printf("%s ", msg);
-         rc = scanf("%lld%c", &value, &excess);
-         if (rc == 0) 
+         printf("%s", msg);
+         count = 0;
+         rc =0;
+         fgets(val,20,stdin);
+         if(val[0]!='\n')
          {
-             printf("**No input accepted!**\n\n");
-             clear_buffer();
-         } 
-         else if (excess != '\n') 
-         {
-             printf("**Trailing characters!**\n\n"); 
-             clear_buffer();
-         } 
-         else if (value < min || value > max) 
-         {
-             printf("**Out of range!**\n\n");
-         } 
-         else
-             keeptrying = 0;
-         } 
-     while (keeptrying == 1);
-     
-     return value;
+             if(0<max&&0>min) if(val[0]=='0' && val[1] == '\n') return 0;
+             for(int i=0; i< 20; i++) 
+             {
+                 if(val[i]=='\n') rc = 1;
+             } 
+             if(rc==0) clear_buffer();
+             intval = atoi(val);
+ 
+             //printf("%d\n", intval);
+ 
+             if(intval<=0) count++;
+ 
+             while(intval!=0)
+             {
+                 intval/=10;
+                 count++;
+             }
+ 
+             //printf("%d\n", count);
+ 
+             intval = atoi(val);
+
+             if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
+             else if(min<intval&&intval<max)
+             {
+                 return intval;
+             }else printf("**OUT OF RANGE**\n");
+         } else printf("**No input**\n");
+     }
  }
 
  double getDouble(char msg[], double min, double max) 
