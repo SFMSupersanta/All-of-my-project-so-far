be13d5f46279847ee8bca64964997f554cc063e5
SFMSupersanta <otakuaria4710@gmail.com>
2022-03-04 23:41:08 +0700

    Validator header update.
    - Removed getOdd and getEven.
    - getInt now can take a number even if it starts by 0s

--- Headers/validator.h
+++ Headers/validator.h
@@ -1,5 +1,6 @@
 
  #include<stdlib.h>
+ #include<string.h>
  
  /* clear empties input buffer */ 
  void clear_buffer (void)
@@ -38,94 +39,7 @@
      }
  }
 
- //odd val
- long long getOdd(char arr[],long long min, long long max)
-{
-    char val[20];           //input value
-    long long intval;
-    int count,rc;      //int value and count value
-    while(1)
-    {
-        printf("%s", arr);
-        count = 0;
-        rc =0;
-        fgets(val,20,stdin);
-        if(val[0]!='\n')
-        {
-            if(0<=max&&0>=min) if(val[0]=='0' && val[1] == '\n') return 0;
-            for(int i=0; i< 20; i++) 
-            {
-                if(val[i]=='\n') rc = 1;
-            } 
-            if(rc==0) clear_buffer();
-            char *ovf;
-
-            intval = strtoll(val,&ovf,10);
-
-            long long intvaldup = intval;
-
-            if(intval<=0) count++;
-
-            while(intvaldup!=0)
-            {
-                intvaldup/=10;
-                count++;
-            }
-
-            if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
-            else if(min<intval&&intval<max)
-            {
-                if(intval%2!=0) return intval;
-                else printf("**Not an odd number**\n");
-            }else printf("**OUT OF RANGE**\n");
-        } else printf("**No input**\n");
-    }
-}
-
- //even val
- long long getEven(char arr[],long long min, long long max)
- {
-     long long intval;
-     int count,rc;      //int value and count value
-     char val[20];           //input value
-     while(1)
-     {
-         printf("%s", arr);
-         count = 0;
-         rc =0;
-         fgets(val,20,stdin);
-         if(val[0]!='\n')
-         {
-             if(0<=max&&0>=min) if(val[0]=='0' && val[1] == '\n') return 0;
-             for(int i=0; i< 20; i++) 
-             {
-                 if(val[i]=='\n') rc = 1;
-             } 
-             if(rc==0) clear_buffer();
-
-             char *ovf;
-
-             intval = strtoll(val,&ovf,10);
  
-             if(intval<=0) count++;
-
-             long long intvaldup = intval;
- 
-             while(intvaldup!=0)
-             {
-                 intvaldup/=10;
-                 count++;
-             }
-
-             if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
-             else if(min<intval&&intval<max)
-             {
-                 if(intval%2==0) return intval;
-                 else printf("**Not an even number**\n");
-             }else printf("**OUT OF RANGE**\n");
-         } else printf("**No input**\n");
-     }
- }
 
 long long getInt(char msg[], long long min, long long max) 
 {
@@ -136,22 +50,38 @@ long long getInt(char msg[], long long min, long long max)
      {
          printf("%s", msg);
          count = 0;
-         rc =0;
+         rc = 0;
          fgets(val,20,stdin);
 
          if(val[0]!='\n')
          {
              if(0<=max&&0>=min) if(val[0]=='0' && val[1] == '\n') return 0;
-             for(int i=0; i < 20; i++) 
+             for(int i=0; i< 20; i++) 
              {
-                 if(val[i]=='\n') rc = 1;
+                 if(val[i]=='\n') 
+                 {
+                     rc = 1;
+                     i=20;
+                 }
              } 
-             if(rc==0) clear_buffer();
-             char *ovf;
+             if(rc==0) 
+             {
 
+                 clear_buffer();
+             }
+             val[strcspn(val,"\n")] = '\0';
+             
+             if(val[count]=='0') 
+             {
+
+                 while(val[count]=='0')
+                 {
+                     count++;
+                 }
+             }             
+             char *ovf;
              intval = strtoll(val,&ovf,10);
              
- 
              if(intval<=0) count++;
 
              long long intvaldup = intval;
@@ -161,8 +91,7 @@ long long getInt(char msg[], long long min, long long max)
                  intvaldup/=10;
                  count++;
              }
-
-             if (val[count] != '\n')  printf("**Trailing Character(s)**\n");
+             if (val[count] != '\0')  printf("**Trailing Character(s)**\n");
              else if(min<=intval&&intval<=max)
              {
                  return intval;
