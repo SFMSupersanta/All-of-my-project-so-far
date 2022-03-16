b2495db801b32e5eb1fc7d2a910154a59ef7c3d6
SFMSupersanta <otakuaria4710@gmail.com>
2022-03-16 22:08:13 +0700

    Exercises update
    - Added Integer arrays

--- PRF192/Exercises-and-more/test.c
+++ PRF192/Exercises-and-more/test.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
+int pc(int i)
+{
+    if(i<2) return 1;
+    for (int j = 2; j <= sqrt(i); j++)
+    {
+        if(i % j == 0)
+        {
+          return 1;
+        }
+    } return 0;
+}
 
 int main() 
 {
-    char *s = (char*)malloc(sizeof(char) * 17);
-	do
-    {
-        fgets(s, sizeof(s), stdin);
-        s[strcspn(s, "\n")] = '\0';
-        printf("%s\n", s);
-        free(s);
-    }while (s[0] != '\0');
-
-
     
+    printf("number = %d\n",pc(3));
     return(0);
 }
