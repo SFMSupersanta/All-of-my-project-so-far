b83501777f993a864608e9d5b775d29e43b076c8
SFMSupersanta <otakuaria4710@gmail.com>
2022-05-18 22:43:52 +0700

    Java validator

---
+++ PRO192/Testing program folder/test.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class test { 
+    
+    public static void main(String[] args)
+    {
+        int i = validatorInt("Type in an int", -100, 100);
+        System.out.println(i);
+    }
+
+    public static int validatorInt(String ms, int min, int max) //hasNextInt() , hasNextDouble(), regex
+    {
+        Scanner sc  = new Scanner(System.in);
+        
+        int Return = 0;
+        do
+        {
+            System.out.print(ms);
+            while(!sc.hasNextInt())
+            {
+                System.out.println("Please type in an int!");
+                sc.nextLine();
+            }
+            Return  = sc.nextInt();
+            if(Return < min || Return > max) 
+                System.out.println("Out of bound!");
+        }while(Return < min || Return > max);
+        sc.close();
+        return Return;
+    }
+}
